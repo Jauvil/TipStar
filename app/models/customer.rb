3a9bee89ab9db5753286deb4dbf0d11c2995559d
JustinAuvil <justinauvil15@gmail.com>
2017-05-16 14:38:42 -0400

    Refactored show page logic. Need to work on refactoring index logic.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,38 +5,37 @@ class Customer < ActiveRecord::Base
 	validates :address, uniqueness: { scope: :city }
 
 
-    def median_tip(user_id)
-    	tip_array = self.sorted_customer_tips(user_id)
+    def median_tip(user_id = nil)
+        tip_array = self.sorted_customer_tips(user_id)
 		center = tip_array.count / 2
 		if tip_array.count.even?
-           median_tip = tip_array[center - 1]
+            median_tip = tip_array[center - 1]
         else
-           median_tip = tip_array[center]
+            median_tip = tip_array[center]
         end
         median_tip
     end
 
-    def high_tip(user_id)
-        tip_array = self.sorted_customer_tips(user_id)
-    	high_tip = tip_array.last
-    	high_tip
+    def high_tip(user_id = nil)
+            tip_array = self.sorted_customer_tips(user_id)
+    	    high_tip = tip_array.last
+         	high_tip
     end
 
-    def last_tip(user_id)
+    def last_tip(user_id = nil)
     	self.tips.where(user_id: user_id).last
     end
 
-    def sorted_customer_tips(user_id)
-    	customer_tips = self.tips.where(user_id: user_id)
-    	customer_tips.sort_by{ |tip| [tip.dollar, tip.cent] }
+    def sorted_customer_tips(user_id = nil)
+        customer_tips = self.tips.where(user_id: user_id)
+        customer_tips.sort_by{ |tip| [tip.dollar, tip.cent] }
     end
 
-    def self.formatted_price
-    	if self.cent < 10
-    	    self.cent = "0" + self.cent.to_s
-    	else 
-    		self.cent = self.cent
-    	end
+    def street_address
+        self.address.to_s + self.address_two.to_s
     end
-
+    
+    def city_state_zip
+        self.city.to_s + ", " + self.state.to_s + " " + self.zipcode
+    end
 end
--- app/models/tip.rb
+++ app/models/tip.rb
@@ -5,5 +5,12 @@ class Tip < ActiveRecord::Base
   validates :cent, :numericality => { :greater_than_or_equal_to => 0 }
   validates :dollar, :numericality => { :greater_than_or_equal_to => 0 }
 
-  
+    def formatted_tip
+	    if self.cent < 10
+		    "$" + self.dollar.to_s + ".0" + self.cent.to_s
+	    else
+		    "$" + self.dollar.to_s + "." + self.cent.to_s
+        end
+    end
+
 end
